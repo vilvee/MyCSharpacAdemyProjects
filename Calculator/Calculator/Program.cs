ddc705d90f438d433aad708636e51cd0a20d7ca8
Veronika <116470467+vilvee@users.noreply.github.com>
2023-10-08 16:51:12 -0400

    microsoft part 2

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,46 +1,19 @@
-﻿namespace Calculator
+﻿using CalculatorLibrary;
+
+namespace CalculatorProgram
 {
     internal class Program
     {
-        class Calculator
-        {
-            public static double DoOperation(double num1, double num2, string op)
-            {
-                double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-                // Use a switch statement to do the math.
-                switch (op)
-                {
-                    case "a":
-                        result = num1 + num2;
-                        break;
-                    case "s":
-                        result = num1 - num2;
-                        break;
-                    case "m":
-                        result = num1 * num2;
-                        break;
-                    case "d":
-                        // Ask the user to enter a non-zero divisor.
-                        if (num2 != 0)
-                        {
-                            result = num1 / num2;
-                        }
-                        break;
-                    // Return text for an incorrect option entry.
-                    default:
-                        break;
-                }
-                return result;
-            }
-        }
-
+       
         static void Main(string[] args)
         {
             bool endApp = false;
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
+
+            //Initialize Calculator Class
+            Calculator calculator = new ();
 
             while (!endApp)
             {
@@ -83,7 +56,8 @@
 
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    // .DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -103,6 +77,8 @@
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+            // Add call to close the JSON writer before return
+            calculator.Finish();
             return;
         }
     }
